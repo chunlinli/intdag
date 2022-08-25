9845572afd2da72c048bb4c7f72886edefe0b1fe
chunlinli <li000007@umn.edu>
2022-08-25 10:31:49 -0500

    update plot

--- simulation/inference_vs_learning_summary.R
+++ simulation/inference_vs_learning_summary.R
@@ -1,63 +1,157 @@
-
 library(ggplot2)
 library(dplyr)
 
 set.seed(1110)
 
-df1 <- read.csv("simulation/additional_result/hub_iv_sufficient.csv")
-df2 <- read.csv("simulation/additional_result/random_iv_sufficient.csv")
-data1 <- rbind(df1, df2)
-data2 <- data1
-data1$method <- "structure\n learning"
-data1$res1 <- data1$select1
-data1$res15 <- data1$select15
-data2$method <- "inference"
-data2$res1 <- ifelse(data2$pvalue1 < 0.05, 1, 0)
-data2$res15 <- ifelse(data2$pvalue15 < 0.05, 1, 0)
+data1 <- read.csv("simulation/additional_result/hub_iv_sufficient.csv")
+data2 <- read.csv("simulation/additional_result/random_iv_sufficient.csv")
 data <- rbind(data1, data2)
 
-#data$shd <- data_all$shd + 0.05 * rchisq(nrow(data_all), df = 1) # jitter (positive for log transform)
+df_lambda <- data %>% summarise(
+  lambda1 = mean(lambda1),
+  lambda2 = mean(lambda2),
+  lambda3 = mean(lambda3),
+  lambda4 = mean(lambda4),
+  lambda5 = mean(lambda5)
+)
+
+df1_1 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select1_2, na.rm = TRUE))
+df1_1$method <- paste0(
+  "selection: ",
+  formatC(df_lambda[[1]], format = "f", digits = 3)
+)
+
+df1_2 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select1_2, na.rm = TRUE))
+df1_2$method <- paste0(
+  "selection: ",
+  formatC(df_lambda[[2]], format = "f", digits = 3)
+)
+
+df1_3 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select1_3, na.rm = TRUE))
+df1_3$method <- paste0(
+  "selection: ",
+  formatC(df_lambda[[3]], format = "f", digits = 3)
+)
+
+df1_4 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select1_4, na.rm = TRUE))
+df1_4$method <- paste0(
+  "selection: ",
+  formatC(df_lambda[[4]], format = "f", digits = 3)
+)
+
+df1_5 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select1_5, na.rm = TRUE))
+df1_5$method <- paste0(
+  "selection: ",
+  formatC(df_lambda[[5]], format = "f", digits = 3)
+)
+
+df1_select <- rbind(df1_1, df1_2, df1_3, df1_4, df1_5)
+
+df1_test <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(pvalue1 < 0.05, na.rm = TRUE))
+df1_test$method <- "testing"
+
+df1 <- rbind(df1_select, df1_test)
 
 fig_dir <- "simulation/fig"
 if (!dir.exists(fig_dir)) dir.create(fig_dir)
 fig_name <- "additional"
 
-library(scales)
-df <- data %>%
-  group_by(n, graph,method,hypothesis) %>%
-  summarise(res = mean(res1, na.rm = TRUE))
+print(df1, n = 160)
 
-
-
-output <- ggplot(df, aes(n, res, group=interaction(graph,method), colour = method)) +
+output <- ggplot(df1, aes(n, res, group = interaction(graph, method), colour = method)) +
   facet_wrap(~hypothesis) +
   geom_point(aes(shape = graph), size = 2.5) +
   geom_line() +
-  #scale_y_continuous(trans = log2_trans(), breaks = c(1, 2, 4, 32, 512)) +
   labs(y = "power", x = "sample size n", shape = paste0("graph type")) +
   theme_bw() +
-  ggtitle("p = 50, q = 100") + theme(plot.title = element_text(size=11))
+  ggtitle("p = 50, q = 100") +
+  theme(plot.title = element_text(size = 11))
 
 output
 
-ggsave(sprintf("%s/%s.pdf", fig_dir, fig_name), output, width = 8, height = 5)
+ggsave(sprintf("%s/%s_1.pdf", fig_dir, fig_name), output, width = 8, height = 5)
 
 
 
-df3 <- data %>%
-  group_by(n, graph, magnitude, method, hypotheis) %>%
-  summarise(shd_mean = mean(shd_nuisance, na.rm = TRUE))
 
 
+df15_1 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select15_2, na.rm = TRUE))
+df15_1$method <- paste0(
+  "kappa = ",
+  formatC(1, format = "d")
+)
 
-print(df3, n = 130)
+df15_2 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select15_2, na.rm = TRUE))
+df15_2$method <- paste0(
+  "kappa = ",
+  formatC(2, format = "d")
+)
 
+df15_3 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select15_3, na.rm = TRUE))
+df15_3$method <- paste0(
+  "kappa = ",
+  formatC(3, format = "d")
+)
 
-# fdr
-df1 <- data1 %>%
-  group_by(n, graph,magnitude) %>%
-  summarise(fdr_mean = mean(fdr, na.rm = TRUE))
-print(df1, n=80)
+df15_4 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select15_4, na.rm = TRUE))
+df15_4$method <- paste0(
+  "kappa = ",
+  formatC(4, format = "d")
+)
 
+df15_5 <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(select15_5, na.rm = TRUE))
+df15_5$method <- paste0(
+  "kappa = ",
+  formatC(5, format = "d")
+)
 
+df15_select <- rbind(df15_1, df15_2, df15_3, df15_4, df15_5)
+
+df15_test <- data %>%
+  group_by(n, graph, hypothesis) %>%
+  summarise(res = mean(pvalue15 < 0.05, na.rm = TRUE))
+df15_test$method <- "alpha = 0.05"
+
+df15 <- rbind(df15_select, df15_test)
+
+fig_dir <- "simulation/fig"
+if (!dir.exists(fig_dir)) dir.create(fig_dir)
+fig_name <- "additional"
+
+print(df15, n = 160)
+
+output <- ggplot(df15, aes(n, res, group = interaction(graph, method), colour = method)) +
+  facet_wrap(~hypothesis) +
+  geom_point(aes(shape = graph), size = 2.5) +
+  geom_line() +
+  labs(y = "rejection rate", x = "sample size n", shape = paste0("graph type")) +
+  theme_bw() +
+  ggtitle("p = 50, q = 100") +
+  theme(plot.title = element_text(size = 11))
+
+output
+
+ggsave(sprintf("%s/%s_15.pdf", fig_dir, fig_name), output, width = 10, height = 5)
 
